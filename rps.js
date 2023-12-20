ffba601ac3d21fa8e2d6d2e839a8cb5605c76f32
Adrien <132284963+Dereniv@users.noreply.github.com>
2023-12-21 03:15:51 +0400

    Add basic UI to the game
    Give a basic UI to the game, the user can define how many round they
    need to win in a Best Of format. The game gives basic feedback on each
    round and annouces the winner at the end.

--- rps.js
+++ rps.js
@@ -1,65 +1,96 @@
 function getComputerChoice() {
-    const list = ["ROCK", "PAPER", "SCISSORS"];
+    const list = ["Rock", "Paper", "Scissors"];
     let index = Math.floor(Math.random() * 3);
     return list[index];
 }
 
-function playRPS(playerSelection, computerSelection) {
-    let computer_choice = computerSelection;
-    let player_choice = playerSelection;
-    let p_win;
+function playRPS(player_choice, computer_choice) {
     if (computer_choice == player_choice) {
-        return [`Computer: ${computer_choice}, Player: ${player_choice},
-                 Draw! We replay the round:`, (p_win = "draw")];
+        return "draw";
     } 
-    else if ((player_choice == "ROCK" && computer_choice == "SCISSORS") ||
-             (player_choice == "PAPER" && computer_choice == "ROCK") ||
-             (player_choice == "SCISSORS" && computer_choice == "PAPER")) {
-        return [`Computer: ${computer_choice}, Player: ${player_choice},
-                 you win!`, (p_win = "yes")];
+    else if ((player_choice == "Rock" && computer_choice == "Scissors") ||
+             (player_choice == "Paper" && computer_choice == "Rock") ||
+             (player_choice == "Scissors" && computer_choice == "Paper")) {
+        return "yes";
     } 
-    else if ((computer_choice == "ROCK" && player_choice == "SCISSORS") ||
-             (computer_choice == "PAPER" && player_choice == "ROCK") ||
-             (computer_choice == "SCISSORS" && player_choice == "PAPER")) {
-        return [`Computer: ${computer_choice}, Player: ${player_choice},
-                you lose!`, (p_win = "no")];
+    else if ((computer_choice == "Rock" && player_choice == "Scissors") ||
+             (computer_choice == "Paper" && player_choice == "Rock") ||
+             (computer_choice == "Scissors" && player_choice == "Paper")) {
+        return "no";
     } else {
-        return ["An error occured, try again", (p_win = "error")];
+        return "error";
     }
 }
 
-function game() {
-    let rounds = +prompt("How many points to win?");
-    let c_points = 0,
-    p_points = 0;
-    while ((c_points || p_points) < rounds) {
-        let playerSelection = prompt("Do you take ROCK, PAPER or SCISSORS?");
-        playerSelection = playerSelection.toUpperCase();
-        let computerSelection = getComputerChoice();
-        [str_result, result] = playRPS(playerSelection, computerSelection);
-        console.log(str_result);
-        switch (result) {
-            case "draw":
-                break;
-            case "yes":
-                p_points++;
-                break;
-            case "no":
-                c_points++;
-                break;
-            default:
-                console.log("error");
-                i = round;
-                p_points = c_points;
-                break;
-        }
-        console.log(`p_points: ${p_points}, c_points: ${c_points}`);
-    }
-    if (p_points > c_points) {
-        console.log("Congratulation, you won the game!");
-    } else if (c_points > p_points) {
-        console.log("Sorry, you lost the game...");
-    } else {
-        console.log("Sorry, an error occured during the game");
-    }
-}  
+let displayChoice = function() {
+    // create a selection panel 
+    const div_choice = document.createElement('div');
+    const div_result = document.createElement('div');
+    const btn_r = document.createElement('button');
+    const btn_p = document.createElement('button');
+    const btn_s = document.createElement('button');
+    btn_r.classList.add("rps");
+    btn_p.classList.add("rps");
+    btn_s.classList.add("rps");
+    div_choice.classList.add("choice");
+    btn_r.textContent = "Rock";
+    btn_p.textContent = "Paper";
+    btn_s.textContent = "Scissors";
+    document.body.appendChild(div_choice);
+    div_choice.appendChild(btn_r);
+    div_choice.appendChild(btn_p);
+    div_choice.appendChild(btn_s);
+    div_choice.appendChild(div_result);
+    
+    // create the feedback panel
+    const p_result = document.createElement('p');
+    const p_score = document.createElement('p');
+    const p_feedback  = document.createElement('p');
+    p_result.classList.add('result');   
+    p_score.classList.add('score');   
+    p_feedback.classList.add('result');   
+    div_choice.appendChild(p_result);
+    div_choice.appendChild(p_score);
+    div_choice.appendChild(p_feedback);
+
+    // Set up the rounds to play and win count
+    const rounds = +prompt("How many points to win?");
+    let computer_win = 0;
+    let player_win = 0;
+
+    // Play a round and give feedback
+    btn_choice = document.querySelectorAll(".rps");
+    btn_choice.forEach( (b) => {
+        b.addEventListener('click', () => {
+            let player_choice = b.textContent;
+            let computer_choice = getComputerChoice();
+            let result = playRPS(player_choice, computer_choice);
+            p_result.textContent = `${player_choice} VS ${computer_choice}`;
+            switch (result) {
+                case "draw":
+                    p_feedback.textContent = `Draw!`;
+                    break;
+                case "yes":
+                    p_feedback.textContent = `You Win!`;
+                    player_win++;
+                    break;
+                case "no":
+                    p_feedback.textContent = `You Lose!`;
+                    computer_win++;
+                    break;
+                default:
+                    p_feedback.textContent = `Sorry, an error occured during the game`;
+                    break;
+            }
+            p_score.textContent = `Player: ${player_win} | Computer: ${computer_win}`; 
+            if (player_win == rounds) {
+                p_feedback.textContent = `Congratulation, you won the game!`;
+            } else if (computer_win == rounds) {
+                p_feedback.textContent = `Sorry, you lost the game :(`;
+            };
+        });
+    });
+}
+
+const btn_play = document.querySelector("#play");
+btn_play.addEventListener('click', displayChoice);
